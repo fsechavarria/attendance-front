618fe9fba74998bad8510a577199c393aa0ab138
fsechavarria <fsechavarria@gmail.com>
2019-08-10 17:10:24 -0400

    Removed auto id generation from routes to prevent O(n^2)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,18 @@
 import * as paths from './paths';
 
-import { generateRouteIds } from '../utils';
 import * as views from '../views';
 
 const routes = [
   {
+    id: 1,
     path: paths.HOME,
     component: views.Home
+  },
+  {
+    id: 2,
+    path: paths.CLIENTS_LIST,
+    component: views.Clients
   }
 ];
 
-const routesWithIds = routes.map(route => generateRouteIds(route));
-
-export default routesWithIds;
+export default routes;
