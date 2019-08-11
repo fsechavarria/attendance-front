2080b9bdbe2451cf8352eebce21c188e7c79999e
fsechavarria <fsechavarria@gmail.com>
2019-08-10 20:36:26 -0400

    Added data types

---
+++ src/views/Clients/components/ClientsListTable/constants.js
@@ -0,0 +1,13 @@
+export const NAME = 'name';
+export const EMAIL = 'email';
+export const PLAN_TYPE = 'planType';
+export const NEXT_BILLING_DATE = 'nextBillingDate';
+export const LAST_ATTENDED_CLASS = 'lastAttendedClass';
+
+export const LABELS = {
+  [NAME]: 'Nombre',
+  [EMAIL]: 'Email',
+  [PLAN_TYPE]: 'Tipo de Plan',
+  [NEXT_BILLING_DATE]: 'Fecha próximo pago',
+  [LAST_ATTENDED_CLASS]: 'Fecha última clase'
+};
